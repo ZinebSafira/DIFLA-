8791f20ffc6d069d29cfac6a174704cb9bc3f5dd
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-05-28 20:03:35 +0200

    update animation

--- src/components/LevelBubble.jsx
+++ src/components/LevelBubble.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import LockIcon from "../assets/lock-icon.png";
 
 function LevelBubble({ fishImage, isLocked, style, levelId, onClick }) {
+  const bubbleRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(bubbleRef.current, {
+      y: "+=20",
+      duration: 2,
+      yoyo: true,
+      repeat: -1,
+      ease: "power1.inOut",
+    });
+  }, []);
+
   const handleClick = () => {
     if (!isLocked) {
       onClick(levelId);
@@ -9,7 +22,11 @@ function LevelBubble({ fishImage, isLocked, style, levelId, onClick }) {
   };
 
   return (
-    <div className="level-bubble" style={style} onClick={handleClick}>
+    <div
+      ref={bubbleRef}
+      className="level-bubble"
+      style={style}
+      onClick={handleClick}>
       <img src={fishImage} alt="Fish" className="w-24 h-24" />
       {isLocked && (
         <img
