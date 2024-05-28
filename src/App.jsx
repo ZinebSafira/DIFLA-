33ceeab645daf0c97061db66362e80dd0db52908
Navisa Sabeti <navisasabeti10@gmail.com>
2024-05-28 11:51:22 +0200

    fixed the issue with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import LevelSelectorPage from "./pages/LevelSelectorPage"; // Import your page component
+import LevelSelector from "./components/LevelSelector"
 import './pages/LevelSelector.css'; // I forgot to import this and I was like what is going on?
 
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/levels" element={<LevelSelectorPage />} />
+        <Route path="/levels" element={<LevelSelector />} />
         {/* Other team members will add their routes here */}
       </Routes>
     </div>
--- src/pages/LevelSelectorPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LevelSelector from "../components/LevelSelector";
-
-function LevelSelectorPage() {
-  return <LevelSelector />;
-}
-
-export default LevelSelectorPage;
