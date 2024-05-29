e29a7ac163dec3e1b40ebb62d67c4e142a87b6ad
MaikHenda <73967823+MaikHenda@users.noreply.github.com>
2024-05-29 12:57:10 +0200

    Background gefixt

---
+++ src/StarterProfile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './StarterProfile.css';
+export default function StarterProfile(){
+return(
+<div className="container">
+      {/* Your content goes here */}
+      <h1>Hello, world!</h1>
+    </div>
+)
+}
